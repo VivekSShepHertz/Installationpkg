1ce06f1080441ed31312d42d4e8765dc46ca063b
vivek soni <vivek.soni@shephertz.co.in>
2017-10-31 13:05:25 +0000

    update

--- postgresql-proxyha1/settingup.sh
+++ postgresql-proxyha1/settingup.sh
@@ -10,7 +10,7 @@ domain=`sudo cat /etc/resolv.conf |grep search|awk '{print $2}'`
 #sudo debconf-set-selections <<< "postfix postfix/mailname string ${host}.${domain}"
 #sudo debconf-set-selections <<< "postfix postfix/main_mailer_type string 'Internet Site'"
 
-sudo apt-get install -y wget ca-certificates i
+sudo apt-get install -y wget ca-certificates 
 #postfix mutt mailutils
 
 		if [ $? -eq 0 ]; then
