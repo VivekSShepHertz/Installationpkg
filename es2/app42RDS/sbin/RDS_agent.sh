ab6c19625277a9132ce429543ee040c491ecba53
StorageCli <root@storagecli.storagecli.t10.internal.cloudapp.net>
2017-11-17 18:44:56 +0530

    Update Agent

--- es2/app42RDS/sbin/RDS_agent.sh
+++ es2/app42RDS/sbin/RDS_agent.sh
@@ -13,8 +13,8 @@ get.system.info)
         echo '{"code":5000,"success":"true","message":"Current System Info", "CPU":"'$used_cpu'", "Memory":"'$mem_percent'", "Load Avg":"'$load'"}'
                 ;;
 
-get.redis.info)
-                d=`netstat -npl|grep redis-server|grep 6379|head -1|rev|awk '{print $1}'|cut -d '/' -f2`
+get.es.info)
+                d=`netstat -npl|grep java|grep 9200|head -1|rev|awk '{print $1}'|cut -d '/' -f2`
                 /bin/echo "d=$d"
                 if [ ! -z $d ]; then
                         used_cpu=`top -b -n1 | grep "Cpu(s)" | awk '{print $2 + $4}'`
@@ -22,16 +22,19 @@ get.redis.info)
                         total_mem=`top -b -n1 | grep "Mem"|awk '{print $2}'|cut -d"k" -f1`
                         used_mem=`top -b -n1 | grep "Mem"|awk '{print $4}'|cut -d"k" -f1`
                         mem_percent=`echo "$used_mem * 100 / $total_mem"|bc`
-                        dsk=`df  -T|grep "/var/lib/redis"|awk '{print $3}'`
+                        dsk=`df  -T|grep "/var/lib/elasticsearch"|awk '{print $3}'`
                         disk_MB=`echo "$dsk / 1000 + 50"|bc`
                         load=`uptime |rev|cut -d ',' -f3|rev|cut -d ':' -f2|tr -d ' '`
-                       #conn_stat=`mysql -u root -pApp42ShepAdmin  -e 'SHOW STATUS WHERE variable_name LIKE "Threads_%" OR variable_name = "Connections"' 2> /tmp/err`
-                       # conn=`echo $conn_stat|grep "Threads_connected"|awk '{print $8}'`
-                       conn=`echo "INFO" | redis-cli -a $2 | grep connected_clients|cut -d ':' -f2|tr -d '\r'`
-                       #cache=`echo $conn_stat|grep "Threads_cached"|awk '{print $6}'`
-                       #max_conn_stat=`mysql -u root -pApp42ShepAdmin  -e 'SHOW VARIABLES LIKE "max_connections"' 2> /tmp/err1`
-                       #max_conn=`echo $max_conn_stat|grep "max_connections"|awk '{print $4}'`
-                        max_conn=`echo "config get maxclients"|redis-cli -a $2|tail -1`
+
+                        nodekey=`curl -u elastic:App42ElasticRDS http://localhost:9200/_nodes/thread_pool\?pretty|/app42RDS/sbin/jq-linux64 '.nodes | keys[]'|head -1`
+
+                        esconn=`curl -u elastic:App42ElasticRDS http://localhost:9200/_nodes/thread_pool\?pretty|/app42RDS/sbin/jq-linux64 '.nodes.'$nodekey'.thread_pool.index.queue_size'`
+                        max_conn=`echo "$esconn * 4"|bc`
+                        es1=`curl -XGET -u elastic:App42ElasticRDS 'localhost:9200/_cat/thread_pool?pretty'|grep "es1"|grep bulk|awk '{print $3}'`
+                        es2=`curl -XGET -u elastic:App42ElasticRDS 'localhost:9200/_cat/thread_pool?pretty'|grep "es2"|grep bulk|awk '{print $3}'`
+                        es3=`curl -XGET -u elastic:App42ElasticRDS 'localhost:9200/_cat/thread_pool?pretty'|grep "es3"|grep bulk|awk '{print $3}'`
+                        es4=`curl -XGET -u elastic:App42ElasticRDS 'localhost:9200/_cat/thread_pool?pretty'|grep "es4"|grep bulk|awk '{print $3}'`
+                        conn=`echo "$es1 + $es4 + $es3 + $es4"|bc`
                         if [ -z "$conn" ]; then
                                 err=`cat /tmp/err |grep "ERROR"|awk '{print $2}'`
                                 if [ 1040 -eq $err ]; then
@@ -65,13 +68,15 @@ get.system.memory)
         echo '{"code":5000,"success":"true","message":"Current System Memory Usages","Memory":"'$mem_percent'"}'
         ;;
 
-get.redis.connection)
-                d=`netstat -npl|grep redis-server|grep 6379|head -1|rev|awk '{print $1}'|cut -d '/' -f2`
+get.es.connection)
+                d=`netstat -npl|grep java|grep 9200|head -1|rev|awk '{print $1}'|cut -d '/' -f2`
                 /bin/echo "d=$d"
                 if [ ! -z $d ]; then
-                #       conn_stat=`mysql -u root -pApp42ShepAdmin  -e 'SHOW STATUS WHERE variable_name LIKE "Threads_%" OR variable_name = "Connections"' 2> /tmp/err`
-                #       conn=`echo $conn_stat|grep "Threads_connected"|awk '{print $8}'`
-                        conn=`echo "INFO" | redis-cli -a $2 | grep connected_clients|cut -d ':' -f2|tr -d '\r'`
+                        es1=`curl -XGET -u elastic:App42ElasticRDS 'localhost:9200/_cat/thread_pool?pretty'|grep "es1"|grep bulk|awk '{print $3}'`
+                        es2=`curl -XGET -u elastic:App42ElasticRDS 'localhost:9200/_cat/thread_pool?pretty'|grep "es2"|grep bulk|awk '{print $3}'`
+                        es3=`curl -XGET -u elastic:App42ElasticRDS 'localhost:9200/_cat/thread_pool?pretty'|grep "es3"|grep bulk|awk '{print $3}'`
+                        es4=`curl -XGET -u elastic:App42ElasticRDS 'localhost:9200/_cat/thread_pool?pretty'|grep "es4"|grep bulk|awk '{print $3}'`
+                        conn=`echo "$es1 + $es4 + $es3 + $es4"|bc`
                         if [ -z "$conn" ]; then
                                 err=`cat /tmp/err |grep "ERROR"|awk '{print $2}'`
                                 if [ 53300 -eq $err ]; then
@@ -91,11 +96,14 @@ get.system.load)
         ;;
 
 
-get.redis.max.connection)
-                d=`netstat -npl|grep redis-server|grep 6379|head -1|rev|awk '{print $1}'|cut -d '/' -f2`
+get.es.max.connection)
+                d=`netstat -npl|grep java|grep 9200|head -1|rev|awk '{print $1}'|cut -d '/' -f2`
                 /bin/echo "d=$d"
                 if [ ! -z $d ]; then
-                        max_conn=`echo "config get maxclients"|redis-cli -a $2|tail -1`
+                        nodekey=`curl -u elastic:App42ElasticRDS http://localhost:9200/_nodes/thread_pool\?pretty|/app42RDS/sbin/jq-linux64 '.nodes | keys[]'|head -1`
+
+                        esconn=`curl -u elastic:App42ElasticRDS http://localhost:9200/_nodes/thread_pool\?pretty|/app42RDS/sbin/jq-linux64 '.nodes.'$nodekey'.thread_pool.index.queue_size'`
+                        max_conn=`echo "$esconn * 4"|bc`
                         echo '{"code":5000,"success":"true","message":"Current Max Connection Set on Redis","Max Connection":"'$max_conn'"}'
                 else
                         echo '{"success":"false","code":3001, "message":"Current Max Connection Set on Redis Could Not Be Fetch, Due To Redis Not Running"}'
@@ -103,197 +111,40 @@ get.redis.max.connection)
         ;;
 
 
-get.current.master)
-
-        master=`echo "INFO" | redis-cli -a $2 | grep role|cut -d ':' -f2|tr -d '\r'`
-        if [ $master == slave ]; then
-                master=`echo "INFO" | redis-cli -a $2 | grep master_host|cut -d ':' -f2|tr -d '\r'`
-        fi
-        if [ $master == master ]; then
-                master=`ip \r|grep "proto kernel  scope link  src"|rev|awk '{print $1}'|rev`
-        fi
-
-        if [ ! -z $master ]; then
-                echo '{"code":5000,"success":"true","message":"Current Redis Master","Master":"'$master'"}'
-        else
-                echo '{"success":"false","code":3001, "message":"We Cannot Find Redis Master"}'
-
-
-        fi
-        ;;
-
-
-get.current.slave)
-        slave=`echo "INFO" | redis-cli -a $2 | grep role|cut -d ':' -f2|tr -d '\r'`
-        if [ $slave == slave ]; then
-                slave=`ip \r|grep "proto kernel  scope link  src"|rev|awk '{print $1}'|rev`
-        fi
-        if [ $slave == master ]; then
-                slave=`echo "INFO" | redis-cli -a $2 | grep slave0|cut -d '=' -f2|cut -d ',' -f1|tr -d '\r'`
-        fi
-        if [ ! -z $slave ]; then
-                echo '{"code":5000,"success":"true","message":"Current Redis Slave","Slave":"'$slave'"}'
-
-                else
-                        echo '{"success":"false","code":3001, "message":"We Cannot Find Redis Slave"}'
-
-        fi
-        ;;
-
-get.failover.agent.status)
-        p1=`ssh -i /root/.ssh/id_rsa root@10.20.1.5 ps ax |grep sentinel|grep -v grep|awk '{print $1}'`
-        p2=`ssh -i /root/.ssh/id_rsa root@10.20.1.6 ps ax |grep sentinel|grep -v grep|awk '{print $1}'`
-        r1=`ssh -i /root/.ssh/id_rsa root@10.20.1.7 ps ax |grep sentinel|grep -v grep|awk '{print $1}'`
-        r2=`ssh -i /root/.ssh/id_rsa root@10.20.1.8 ps ax |grep sentinel|grep -v grep|awk '{print $1}'`
-
-        if [ ! -z $p1 ] && [ ! -z $p2 ] && [ ! -z $r1 ] && [ ! -z $r2 ]; then
-                echo '{"code":5000,"success":"true","message":"Sentinel Process Is Running"}'
-        else
-                echo '{"success":"false","code":3001, "message":"Sentinel Process Is Not Running"}'
-        fi
-        ;;
-
-
-
-run.failover)
-        master=`echo "INFO" | redis-cli -a $2 | grep role|cut -d ':' -f2|tr -d '\r'`
-        if [ $master == slave ]; then
-                master=`echo "INFO" | redis-cli -a $2 | grep master_host|cut -d ':' -f2|tr -d '\r'`
-        fi
-        if [ $master == master ]; then
-                master=`ip \r|grep "proto kernel  scope link  src"|rev|awk '{print $1}'|rev`
-        fi
-
-        slave=`echo "INFO" | redis-cli -a $2 | grep role|cut -d ':' -f2|tr -d '\r'`
-        if [ $slave == slave ]; then
-                slave=`ip \r|grep "proto kernel  scope link  src"|rev|awk '{print $1}'|rev`
-        fi
-        if [ $slave == master ]; then
-                slave=`echo "INFO" | redis-cli -a $2 | grep slave0|cut -d '=' -f2|cut -d ',' -f1|tr -d '\r'`
-        fi
-
-        p1=`ssh -i /root/.ssh/id_rsa root@10.20.1.5 ps ax |grep sentinel|grep -v grep|awk '{print $1}'`
-        p2=`ssh -i /root/.ssh/id_rsa root@10.20.1.6 ps ax |grep sentinel|grep -v grep|awk '{print $1}'`
-        r1=`ssh -i /root/.ssh/id_rsa root@$master ps ax |grep sentinel|grep -v grep|awk '{print $1}'`
-        r2=`ssh -i /root/.ssh/id_rsa root@$slave ps ax |grep sentinel|grep -v grep|awk '{print $1}'`
-
-        if [ -z $p1 ] && [ -z $p2 ] && [ -z $r2 ]; then
-                echo '{"success":"false","code":3001, "message":"Not Running Sentinel On Any VM, We Can Not Run Redis Failover"}'
-                exit 1
-        fi
-
-        ssh -i /root/.ssh/id_rsa root@"$master" sed -i '/check_db/d' /etc/crontab
-        ssh -i /root/.ssh/id_rsa root@"$master" /etc/init.d/redis stop $2
-        count=1
-        while [ $count -lt 16 ]; do
-                new_master=`ssh -i /root/.ssh/id_rsa root@$slave echo "INFO" | redis-cli -a $2 | grep role|cut -d ':' -f2|tr -d '\r'`
-                if [ $new_master == master ]; then
-                        count=17
-                        ssh -i /root/.ssh/id_rsa root@"$master" sed -i '/check_db/d' /etc/crontab
-                        ssh -i /root/.ssh/id_rsa root@"$master" 'echo "*/2     *       *       *       *       root    /app42RDS/sbin/check_db" >> /etc/crontab'
-                        echo '{"code":5000,"success":"true","message":"Redis Failover Completed Successfully","New Master":"'$slave'"}'
-                        exit 0
-                else
-                        count=$((count+1))
-                        sleep 30
-                fi
-        done
-        ssh -i /root/.ssh/id_rsa root@"$master" sed -i '/check_db/d' /etc/crontab
-        ssh -i /root/.ssh/id_rsa root@"$master" 'echo "*/2     *       *       *       *       root    /app42RDS/sbin/check_db" >> /etc/crontab'
-        ssh -i /root/.ssh/id_rsa root@"$master" /etc/init.d/redis restart $2
-        echo '{"success":"false","code":3001, "message":"Redis Failover Could Not Be Succeed"}'
-        ;;
-
 update.max.connection)
-        master=`echo "INFO" | redis-cli -a $2 | grep role|cut -d ':' -f2|tr -d '\r'`
-        if [ $master == slave ]; then
-                master=`echo "INFO" | redis-cli -a $2 | grep master_host|cut -d ':' -f2|tr -d '\r'`
-        fi
+sed -i s/'queue_size: 500'/'queue_size: 2500'/g /etc/elasticsearch/elasticsearch.yml
 
-        if [ $master == master ]; then
-                master=`ip \r|grep "proto kernel  scope link  src"|rev|awk '{print $1}'|rev`
-        fi
+        nodekey=`curl -u elastic:App42ElasticRDS http://localhost:9200/_nodes/thread_pool\?pretty|/app42RDS/sbin/jq-linux64 '.nodes | keys[]'|head -1`
 
-        slave=`echo "INFO" | redis-cli -a $2 | grep role|cut -d ':' -f2|tr -d '\r'`
-        if [ $slave == slave ]; then
-                slave=`ip \r|grep "proto kernel  scope link  src"|rev|awk '{print $1}'|rev`
-        fi
-        if [ $slave == master ]; then
-                slave=`echo "INFO" | redis-cli -a $2 | grep slave0|cut -d '=' -f2|cut -d ',' -f1|tr -d '\r'`
-        fi
+        esconn=`curl -u elastic:App42ElasticRDS http://localhost:9200/_nodes/thread_pool\?pretty|/app42RDS/sbin/jq-linux64 '.nodes.'$nodekey'.thread_pool.index.queue_size'`
+        sed -i s/'queue_size: '$esconn''/'queue_size: '$2''/g /etc/elasticsearch/elasticsearch.yml
 
-        mastermaxconn=`ssh -i /root/.ssh/id_rsa root@"$master" cat /etc/redis.conf|grep maxclients|awk '{print $2}'`
-        slavemaxconn=`ssh -i /root/.ssh/id_rsa root@"$master" cat /etc/redis.conf|grep maxclients|awk '{print $2}'`
 
-        ssh -i /root/.ssh/id_rsa root@$master 'sed -i s/"maxclients '$mastermaxconn'"/"maxclients '$3'"/g /etc/redis.conf'
-        ssh -i /root/.ssh/id_rsa root@$slave 'sed -i s/"maxclients '$slavemaxconn'"/"maxclients '$3'"/g /etc/redis.conf'
+        ssh -i /root/.ssh/id_rsa root@10.20.1.5 'sed -i s/"queue_size: '$esconn'"/"queue_size: '$2'"/g /etc/elasticsearch/elasticsearch.yml'
+        ssh -i /root/.ssh/id_rsa root@10.20.1.7 'sed -i s/"queue_size: '$esconn'"/"queue_size: '$2'"/g /etc/elasticsearch/elasticsearch.yml'
+        ssh -i /root/.ssh/id_rsa root@10.20.1.8 'sed -i s/"queue_size: '$esconn'"/"queue_size: '$2'"/g /etc/elasticsearch/elasticsearch.yml'
 
         if [ $? -eq 0 ]; then
-                ssh -i /root/.ssh/id_rsa root@$master /etc/init.d/redis restart $2
-                ssh -i /root/.ssh/id_rsa root@$slave /etc/init.d/redis restart $2
+                /etc/init.d/elasticsearch restart
+                sleep 30
+                ssh -i /root/.ssh/id_rsa root@10.20.1.5 /etc/init.d/elasticsearch restart
+                sleep 30
+                ssh -i /root/.ssh/id_rsa root@10.20.1.7 /etc/init.d/elasticsearch restart
+                sleep 30
+                ssh -i /root/.ssh/id_rsa root@10.20.1.8 /etc/init.d/elasticsearch restart
                 echo '{"code":5000,"success":"true","message":"Redis Max Connection Update Successfully","New Max Connection":"'$3'"}'
         else
                 echo '{"success":"false","code":3001, "message":"Redis Max Connection Updation Failed"}'
         fi
         ;;
 
-get.slave.status)
-        slave=`echo "INFO" | redis-cli -a $2 | grep role|cut -d ':' -f2|tr -d '\r'`
-        if [ $slave == slave ]; then
-                slave=`ip \r|grep "proto kernel  scope link  src"|rev|awk '{print $1}'|rev`
-        fi
-        if [ $slave == master ]; then
-                slave=`echo "INFO" | redis-cli -a $2 | grep slave0|cut -d '=' -f2|cut -d ',' -f1|tr -d '\r'`
-        fi
-
-        master_sync_in_progress=`ssh -i /root/.ssh/id_rsa root@$slave 'echo "INFO" | redis-cli -a '$2' | grep master_sync_in_progress|cut -d ":" -f2|tr -d "\r"'`
-
-                if [ ! -z "$master_sync_in_progress" ]; then
-                        echo '{"code":5000,"success":"true","message":"Current Redis Slave Status","Seconds Behind Master":"'$master_sync_in_progress'"}'
-                else
-                        sleep 1
-                        echo '{"success":"false","code":3001, "message":"Current Redis Slave Status Could Not Be Fetch, Due To Redis Not Running"}'
-                fi
-        ;;
-
 update.user.password)
-        oldpassword=$2
+        username=$2
         newpassword=$3
 
-        master=`echo "INFO" | redis-cli -a $2 | grep role|cut -d ':' -f2|tr -d '\r'`
-        if [ $master == slave ]; then
-                master=`echo "INFO" | redis-cli -a $2 | grep master_host|cut -d ':' -f2|tr -d '\r'`
-        fi
-        if [ $master == master ]; then
-                master=`ip \r|grep "proto kernel  scope link  src"|rev|awk '{print $1}'|rev`
-        fi
-
-        slave=`echo "INFO" | redis-cli -a $2 | grep role|cut -d ':' -f2|tr -d '\r'`
-        if [ $slave == slave ]; then
-                slave=`ip \r|grep "proto kernel  scope link  src"|rev|awk '{print $1}'|rev`
-        fi
-        if [ $slave == master ]; then
-                slave=`echo "INFO" | redis-cli -a $2 | grep slave0|cut -d '=' -f2|cut -d ',' -f1|tr -d '\r'`
-        fi
-
-        ssh -i /root/.ssh/id_rsa root@$master 'sed -i s/"'$oldpassword'"/"'$newpassword'"/g /etc/redis.conf'
-        ssh -i /root/.ssh/id_rsa root@$slave 'sed -i s/"'$oldpassword'"/"'$newpassword'"/g /etc/redis.conf'
-        ssh -i /root/.ssh/id_rsa root@$master 'sed -i s/"'$oldpassword'"/"'$newpassword'"/g /etc/redis-sentinel.conf'
-        ssh -i /root/.ssh/id_rsa root@$slave 'sed -i s/"'$oldpassword'"/"'$newpassword'"/g /etc/redis-sentinel.conf'
-        ssh -i /root/.ssh/id_rsa root@10.20.1.5 'sed -i s/"'$oldpassword'"/"'$newpassword'"/g /etc/redis-sentinel.conf'
-        ssh -i /root/.ssh/id_rsa root@10.20.1.6 'sed -i s/"'$oldpassword'"/"'$newpassword'"/g /etc/redis-sentinel.conf'
-        ssh -i /root/.ssh/id_rsa root@10.20.1.5 'sed -i s/"'$oldpassword'"/"'$newpassword'"/g /etc/haproxy/haproxy.cfg'
-        ssh -i /root/.ssh/id_rsa root@10.20.1.6 'sed -i s/"'$oldpassword'"/"'$newpassword'"/g /etc/haproxy/haproxy.cfg'
+        curl -XPOST -u elastic:App42ElasticRDS 'localhost:9200/_xpack/security/user/'$username'/_password' -H "Content-Type: application/json" -d '{ "password" : "'$newpassword'" }'
 
         if [ $? -eq 0 ]; then
-                ssh -i /root/.ssh/id_rsa root@$master /etc/init.d/redis-sentinel restart
-                ssh -i /root/.ssh/id_rsa root@$slave /etc/init.d/redis-sentinel restart
-                ssh -i /root/.ssh/id_rsa root@10.20.1.5 /etc/init.d/redis-sentinel restart
-                ssh -i /root/.ssh/id_rsa root@10.20.1.6 /etc/init.d/redis-sentinel restart
-                ssh -i /root/.ssh/id_rsa root@$master /etc/init.d/redis restart $2
-                ssh -i /root/.ssh/id_rsa root@$slave /etc/init.d/redis restart $2
-                ssh -i /root/.ssh/id_rsa root@10.20.1.5 /etc/init.d/haproxy restart
-                ssh -i /root/.ssh/id_rsa root@10.20.1.6 /etc/init.d/haproxy restart
                 echo '{"code":5000,"success":"true","message":"Redis Password Update Successfully"}'
         else
                 echo '{"success":"false","code":3001, "message":"Current Redis Password Could Not Be Update"}'
